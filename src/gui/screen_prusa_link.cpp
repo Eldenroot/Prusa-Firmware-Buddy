d20f6857e758d2fe92436d483039396cca0ba874
MistrKernnunos <vitezslav.machacek@skaut.cz>
2022-10-06 16:26:51 +0200

    Migrate from old eeprom to configuration_store

--- src/gui/screen_prusa_link.cpp
+++ src/gui/screen_prusa_link.cpp
@@ -10,6 +10,7 @@
 #include <array>
 
 #include "wui_api.h"
+#include "configuration_store.hpp"
 
 #define api_key_format "%s"
 static constexpr size_t PASSWD_STR_LENGTH = PL_API_KEY_SIZE + 1; // don't need space for '%s' and '\0' since PL_API_KEY_SIZE contains '\0' too
@@ -39,7 +40,7 @@ class MI_PL_ENABLED : public WI_SWITCH_OFF_ON_t {
 
 public:
     MI_PL_ENABLED()
-        : WI_SWITCH_OFF_ON_t(eeprom_get_ui8(EEVAR_PL_RUN),
+        : WI_SWITCH_OFF_ON_t(config_store().pl_run.get(),
             string_view_utf8::MakeCPUFLASH((const uint8_t *)label), IDR_NULL, is_enabled_t::yes, is_hidden_t::no) {}
 
 public:
@@ -140,7 +141,7 @@ void ScreenMenuPrusaLink::windowEvent(EventLock /*has private ctor*/, window_t *
             break;
         }
         case MI_PL_ENABLED::EventMask::value:
-            eeprom_set_ui8(EEVAR_PL_RUN, action);
+            config_store().pl_run.set(action);
             notify_reconfigure();
             break;
         default:
